d618ff5abf6c37a38e0faf3fecf34d6d7df07702
euleramon <euleramon@gmail.com>
2017-01-10 12:18:11 +0800

    use AJAX to load data

--- tableplanner/test.js
+++ tableplanner/test.js
@@ -1,59 +1,25 @@
-   var reader = {}; //GLOBAL File Reader object for demo purpose only
-
-
-    /**
-     * Check for the various File API support.
-     */
-    function checkFileAPI() {
-        if (window.File && window.FileReader && window.FileList && window.Blob) {
-            reader = new FileReader();
-			console.log("check ok");
-            return true; 
-        } else {
-            alert('The File APIs are not fully supported by your browser. Fallback required.');
-            return false;
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if (rawFile.readyState === 4) {
+            if (rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                displayContents(allText);
+            }
         }
     }
+    rawFile.send(null);
+}
 
-    /**
-     * read text input
-     */
-    function readText(filePath) {
-		checkFileAPI();
+/**
+ * display content using a basic HTML replacement
+ */
+function displayContents(txt) {
+    var el = document.getElementById('main');
+    el.innerHTML = txt; //display output in DOM
+}
 
-        var output = ""; //placeholder for text output
-        if(filePath.files && filePath.files[0]) {           
-            reader.onload = function (e) {
-                output = e.target.result;
-                displayContents(output);
-            };//end onload()
-            reader.readAsText(filePath.files[0]);
-        }//end if html5 filelist support
-        else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
-            try {
-                reader = new ActiveXObject("Scripting.FileSystemObject");
-                var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
-                output = file.ReadAll(); //text contents of file
-                file.Close(); //close file "input stream"
-                displayContents(output);
-            } catch (e) {
-                if (e.number == -2146827859) {
-                    alert('Unable to access local files due to browser security settings. ' + 
-                     'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' + 
-                     'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"'); 
-                }
-            }       
-        }
-        else { //this is where you could fallback to Java Applet, Flash or similar
-            return false;
-        }       
-        return true;
-    }   
-
-    /**
-     * display content using a basic HTML replacement
-     */
-    function displayContents(txt) {
-        var el = document.getElementById('main'); 
-        el.innerHTML = txt; //display output in DOM
-    }   
+$(document).ready(function () {
+    readTextFile('http://jeff-eu.github.io/tableplanner/table_seat.txt');
+});
